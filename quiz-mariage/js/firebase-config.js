436d8798ed247096232b65ad7bc71bf9cfa5587f
qtessonneau <quentintessonneau@gmail.com>
2026-07-20 21:21:04 +0200

    Update firebase-config.js

--- quiz-mariage/js/firebase-config.js
+++ quiz-mariage/js/firebase-config.js
@@ -9,14 +9,22 @@
  * Exemple : https://votre-site.github.io/quiz-mariage/?admin=aude-guims-2026
  */
 export const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_PROJECT.firebaseapp.com",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef",
+  apiKey: "AIzaSyCF4W_SxGIlUzUEeaHzOoDlqCVWou1ZKp0",
+  authDomain: "quiz-mariage-aude-guims.firebaseapp.com",
+  projectId: "quiz-mariage-aude-guims",
+  storageBucket: "quiz-mariage-aude-guims.firebasestorage.app",
+  messagingSenderId: "1082994682992",
+  appId: "1:1082994682992:web:59737978e5c749e1fcd8b5"
 };
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 /** Code secret pour accéder au panneau admin (?admin=...) */
 export const ADMIN_CODE = "aude-guims-2026";
 
